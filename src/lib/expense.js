7726accd3921bde5373cfe4d14a6f955cca8319d
hyowls <hhvucyh@gmail.com>
2024-06-13 19:41:21 +0900

    json server

--- src/lib/expense.js
+++ src/lib/expense.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const JSON_SERVER_HOST = "http://localhost:4000";
+const JSON_SERVER_HOST = "https://your-json-server.glitch.me";
 
 export const getExpenses = async () => {
   try {
